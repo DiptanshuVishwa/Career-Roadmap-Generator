e703e7c2f397dd185097baad57e0fbac6d273a3d
DiptanshuVishwa <diptanshuvishwa364@gmail.com>
2026-05-28 21:41:38 +0530

    Setup Express server and MongoDb connection

---
+++ backend/server.js
@@ -0,0 +1,37 @@
+import express from "express";
+import cors from "cors";
+import dotenv from "dotenv";
+import connectDb from "./config/db.js";
+import logger from "./middlewares/logger.js";
+
+dotenv.config();
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+connectDb();
+
+app.use(cors());
+app.use(express.json());
+
+app.use(logger);
+
+app.get("/",(req,res)=>{
+    res.send("API is running");
+});
+// testing route
+app.post("/test", (req,res)=>{
+    console.log(req.body);
+    res.json({
+        message: "Data recevied",
+        data: req.body,
+    });
+});
+
+app.listen(PORT, (error)=>{
+    if(!error){
+        console.log("Server is Running at PORT: "+PORT);
+    }else{
+        console.log("Error: "+error);
+    }
+});
